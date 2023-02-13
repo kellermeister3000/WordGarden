6d7b2758a5d0866ad0f3999c2aceb8ce7ce5a0dc
Philip Keller <philipjoelkeller@gmail.com>
2023-02-13 13:45:48 -0800

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Philip Keller on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
